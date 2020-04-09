43e0dd8fd2b6ea3355777d86f7b7708eb0ebab85
Kawamura Takaki <surrealist1927@gmail.com>
2020-04-09 17:04:27 +0900

    no.7 fin completed quiz app

--- app/src/main/java/com/tutorial/quizapp/MainActivity.java
+++ app/src/main/java/com/tutorial/quizapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -136,6 +137,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialogInterface, int i){
                 if(quizCount == QUIZ_COUNT){
                     //結果画面へ移動
+                    Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                    intent.putExtra("RIGHT_ANSWER_COUNT", rightAnswerCount);
+                    startActivity(intent);
                 }else{
                     quizCount++;
                     showNextQuiz();
